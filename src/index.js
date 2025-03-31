50518bf0df821182071c58f1b577d22ac4d18854
ayscd <ayeskacsta@gmail.com>
2025-03-30 23:29:44 -0300

    DONE

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let noTypeWriter = document.querySelector('#challenge-2');
+noTypeWriter.innerHTML = `Ayeska Costa`;
+
+new Typewriter('#challenge-3', {
+    strings: `Ayeska Costa`,
+    autoStart: true,
+})
